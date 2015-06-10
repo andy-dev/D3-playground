0216d3a3fa9fb5991661af99b2653d465b95893a
andy-dev <andres.macedo@gmail.com>
2015-06-10 05:43:02 -0700

    move data to json file instead of ajax call

---
+++ apiVandricoJson.js
@@ -0,0 +1,25 @@
+
+// var getData = function (){
+//   $.ajax({
+//     url:"https://www.kimonolabs.com/api/553hlgto?apikey=fR6lhnsHZjqjFQe7jbdot8lrFEw72evx",
+//     crossDomain: true,
+//     dataType: "jsonp",
+//   })
+//   .done(function(response) {
+//     console.log("success");
+//     returnData(response)
+//     da(response)
+//   })
+//   .fail(function() {
+//     console.log("error");
+//   })
+//   .always(function() {
+//     console.log("complete");
+//   });
+
+//   var returnData = function(data){
+//     myData = data
+//   }
+// };
+
+
--- table.js
+++ table.js
@@ -1,20 +1,28 @@
-// if(!d3.chart) d3.chart ={};
+if(!d3.chart) d3.chart = {};
 
-// d3.chart.table = function(){
-//   var data;
+d3.chart.table = function(){
+  var data; //using js scope
+  var width; // basically these will be our getter and setter
 
-//   function chart(container){
-//     console.log(data)
-//   }
 
-//   chart.data = function(value){
-//     if(!arguments.length) return data;
-//     data = value;
-//     return chart;
-//   }
+  function chart(container){
+    console.log(data)
+  }
 
-//   return chart;
-// }
+  chart.data = function(value){
+    if(!arguments.length) return data;
+    data = value;
+    return chart;
+  }
 
+  chart.width = function(value){
+    if(!arguments.length) return data;
+    width = value;
+    return chart;
+  }
+
+// return chart function
+return chart;
+}
 
 
